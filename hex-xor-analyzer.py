8a9442543bbfcb85227538d6132d6fb95dbb1310
vhulf <45860423+vhulf@users.noreply.github.com>
2024-03-29 14:28:54 -0600

    added binary support, fixed pairSize 4
    Needed to .decode for binary to work, and pairSize 4 would not function due to me erroneously changing the base for XOR operation based on pairSize... pairsize 2-4 should now function only as a display change, should not change the XOR calculation at all!

--- hex-xor-analyzer.py
+++ hex-xor-analyzer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-## TODO :: FIX BINARY FILE READING! (--asText necessary for now!)
+## DONE! :: FIX BINARY FILE READING! (--asText necessary for now!) [needed to add a .decode!]
 ## DONE! :: ADD SUPPORT FOR BRUTING.... how to integrate it?? [SINGLE COMMAND MODE ADDED!]
 ## TODO :: IMPLEMENT A BRUTE FORCING TO GET XOR KEY WHICH EXISTS IN A BIN??? MAYBE SINGLE COMMAND MODE WONT BE USED BY BASH SCRIPT NOW?
 
@@ -14,6 +14,7 @@ def clear():
     _ = call('clear' if name == 'posix' else 'cls')
 
 def splitByXorLenThenHexorem(inputStr, xorHex):
+    # get rid of any spaces or tabs
     inputStr = inputStr.translate(str.maketrans('', '', string.whitespace))
     outputStr = ""
     for _iter in range(0, int(len(inputStr)//len(xorHex))):
@@ -35,9 +36,8 @@ def hexorem(a, b):
         result = "--"
     else:
         result = "~~"
-        base = 8 * args.pairLength
         try:
-            result = str('%x' % (int(a,base)^int(b,base)))
+            result = str('%x' % (int(a,16)^int(b,16)))
         except:
             print("XOR failure!")
 
@@ -53,7 +53,7 @@ def getStrFromInputFile(filePath, asText):
             outputStr = file.read().replace('\n', '')
     else:
         with open(filePath, 'rb') as file:
-            outputStr = binascii.hexlify(file.read())
+            outputStr = binascii.hexlify(file.read()).decode("utf-8")
     
     return str(outputStr)
 
@@ -62,7 +62,7 @@ def outputHelp():
     print(".                                                                                                    .")
     print(".                                     ~~~ Auxiliary Commands ~~~                                     .")
     print(".                                                                                                    .")
-    print(".  -> pairSize [NUM] ... EX: pairSize 2  , sets length of HEX pairs [EFFECTS COMPUTE, must be 2-4]   .")
+    print(".  -> pairSize [NUM] ... EX: pairSize 2  , sets length of HEX pairs [AFFECTS ASCII, must be 2-4]     .")
     print(".  -> blockSize [NUM] ... EX: blockSize 16 , sets pairs per row [DOES NOT EFFECT COMPUTE]            .")
     print(".  -> ascii ... EX: ascii , enables or disables attempted ascii output ('._' = non-ascii char)       .")
     print(".  -> quit ... EX: quit , quits the analysis script (alias: 'exit')                                  .")
@@ -102,26 +102,42 @@ def outputMonitorView(stringForView, pairLength, blockLength, attemptAscii):
     
     print(" ")
 
-### Command for usage by other python libraries! Gunna be utilized to create some "find the key in its own binary" testing
-def XORItAndShowASCII(fileString, xorKey):
-    currentViewString = splitByXorLenThenHexorem(fileString, xorKey)
-    outputMonitorView(currentViewString, 2, 32, True)
+### Command for usage by other scripts! Gunna be utilized to create some "find the key in its own binary" type bruteforcing...
+def XORItAndReturn(fileString, xorKey, pairLen=2, asciiOut=True):
+    workingString = splitByXorLenThenHexorem(fileString, xorKey)
+
+    returnString = ""
+    for _iter in range(0, len(workingString)//pairLen):
+        hexPair = workingString[0:pairLen]
+        if asciiOut and "-" not in hexPair:
+            # attempt to print out in ASCII mode, if it fails print out filler text "._"
+            try:
+                byteString = bytes.fromhex(hexPair)
+                print(byteString.decode("ASCII"), end=" ", flush="True")
+            except:
+                print("._", end=" ", flush="True")
+        else:
+            print(hexPair, end=" ", flush="True")
+        
+        workingString = workingString[pairLen:]
+    
+
     
 
 if __name__ == "__main__":
     import argparse
 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="A tool for analyzing binary data represented as HEX against different XOR key candidates, including attempted ASCII output")
     parser.add_argument("--file", "-f", required=True, help="binary data file... for files which have already been converted to text use --asText or -t")
     parser.add_argument("--pairLength", "-p", required=False, default=2, help="amount of characters to each hex pair in monitoring view (default: 2)")
     parser.add_argument("--blockLength", "-b", required=False, default=16, help="amount of pairs to be output per block in monitoring view (default: 16)")
     parser.add_argument("--asText", "-t", required=False,  default=False, action="store_true", help="for input files which have already been converted into text-based representation, space deliniated OK!")
     parser.add_argument("--noClear", "-n", required=False,  default=False, action="store_true", help="disables terminal clearing, allowing for earlier output to be reviewed via terminal history")
     parser.add_argument("--singleCommand", "-s", required=False,  default=False, help="enables 'just XOR it' mode, will issue a single xor input (or other command) and display output, useful for integration with other tooling (like bash scripts) for brute force searching! (usage: -s FF)")
+    parser.add_argument("--asciiMode", "-a", required=False, default=False, action="store_true", help="enable ascii mode immediately")
     args = parser.parse_args()
 
     quit = False
-    asciiMode = False
 
     fileString = getStrFromInputFile(args.file, args.asText)
 
@@ -145,29 +161,29 @@ if __name__ == "__main__":
             if commandMod:
                 args.pairLength = int(commandMod)
                 currentViewString = splitByXorLenThenHexorem(fileString, latestHexXor)
-                outputMonitorView(currentViewString, args.pairLength, args.blockLength, asciiMode)
+                outputMonitorView(currentViewString, args.pairLength, args.blockLength, args.asciiMode)
             else:
                 print("Something went wrong, did you input a number in the expected format?")
         elif command == "blockSize":
             if commandMod:
                 args.blockLength = int(commandMod)
                 currentViewString = splitByXorLenThenHexorem(fileString, latestHexXor)
-                outputMonitorView(currentViewString, args.pairLength, args.blockLength, asciiMode)
+                outputMonitorView(currentViewString, args.pairLength, args.blockLength, args.asciiMode)
             else:
                 print("Something went wrong, did you input a number in the expected format?")
         elif command == "ascii":
-            asciiMode = False if asciiMode else True
+            args.asciiMode = False if args.asciiMode else True
             currentViewString = splitByXorLenThenHexorem(fileString, latestHexXor)
-            outputMonitorView(currentViewString, args.pairLength, args.blockLength, asciiMode)
+            outputMonitorView(currentViewString, args.pairLength, args.blockLength, args.asciiMode)
         elif command == "help":
             outputHelp()
         else:
             latestHexXor = command
             currentViewString = splitByXorLenThenHexorem(fileString, command)
-            outputMonitorView(currentViewString, args.pairLength, args.blockLength, asciiMode)
+            outputMonitorView(currentViewString, args.pairLength, args.blockLength, args.asciiMode)
         
         if command != "help" and not args.singleCommand:
-            print("currentXOR: " + latestHexXor + ", asciiMode: " + str(asciiMode) + ", pairSize: " + str(args.pairLength) + ", blockSize: " + str(args.blockLength))
+            print("currentXOR: " + latestHexXor + ", ascii: " + str(args.asciiMode) + ", pairSize: " + str(args.pairLength) + ", blockSize: " + str(args.blockLength))
             print(" ")
 
 
